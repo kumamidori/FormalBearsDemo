406f047d1d14ce99322e2688a909bb84f9686e3e
YAMANE Nana <shigematsu.nana@gmail.com>
2018-12-02 07:44:50 +0900

    FormalBears 初期設定を追加

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -1,8 +1,8 @@
 <?php
 namespace Fob\FormalBearsDemo\Module;
 
-use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use FormalBears\Framework\Module\AbstractAppModule;
 
 class AppModule extends AbstractAppModule
 {
@@ -11,8 +11,30 @@ class AppModule extends AbstractAppModule
      */
     protected function configure()
     {
-        $appDir = $this->appMeta->appDir;
-        require_once $appDir . '/env.php';
         $this->install(new PackageModule);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getAppNamespace(): string
+    {
+        return 'Fob\\FormalBearsDemo';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getAppDir(): string
+    {
+        return dirname(__DIR__, 2);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getConfigExtension(): string
+    {
+        return '.yaml';
     }
 }
